fdf30a02c638a998cebec9962775aec72e9a65dd
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-10-13 12:59:27 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -3,42 +3,34 @@
 #include "catch.hpp"
 #include "stats.h"
 
-#include <stdlib.h>
-#include <math.h>
+#include <cmath>
 
 TEST_CASE("reports average, minimum and maximum") {
-    float numberset[] = {1.5, 8.9, 3.2, 4.5};
-    int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    struct Stats computedStats = compute_statistics(numberset, setlength);
+    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
-    REQUIRE(abs(computedStats.average - 4.525) < epsilon);
-    REQUIRE(abs(computedStats.max - 8.9) < epsilon);
-    REQUIRE(abs(computedStats.min - 1.5) < epsilon);
+    REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
+    REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
 TEST_CASE("average is NaN for empty array") {
-    Stats computedStats = compute_statistics(0, 0);
+    auto computedStats = Statistics::ComputeStatistics({});
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
     //Design the REQUIRE statement here.
-    //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
+    //Use http://www.cplusplus.com/reference/cmath/isnan/
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
-    // create additional .c and .h files
-    // containing the emailAlerter, ledAlerter functions
-    alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
-
-    float numberset[] = {99.8, 34.2, 4.5};
-    int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    Stats computedStats = compute_statistics(numberset, setlength);
-
+    EmailAlert emailAlert;
+    LEDAlert ledAlert;
+    std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    
     const float maxThreshold = 10.2;
-    check_and_alert(maxThreshold, alerters, computedStats);
+    StatsAlerter statsAlerter(maxThreshold, alerters);
+    statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
 
-    // need a way to check if both emailAlerter, ledAlerter were called
-    // you can define call-counters along with the functions, as shown below
-    REQUIRE(emailAlertCallCount == 1);
-    REQUIRE(ledAlertCallCount == 1);
+    REQUIRE(emailAlert.emailSent);
+    REQUIRE(ledAlert.ledGlows);
 }
