d9b29b452035436f0df3af4a3fc311b5e0d78fe4
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-10-14 15:17:07 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,34 +2,47 @@
 
 #include "catch.hpp"
 #include "stats.h"
+#include "alerts.h"
 
-#include <cmath>
+#include <stdlib.h>
+#include <math.h>
 
 TEST_CASE("reports average, minimum and maximum") {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+    float numberset[] = {1.5, 8.9, 3.2, 4.5};
+    int setlength = sizeof(numberset) / sizeof(numberset[0]);
+    struct Stats computedStats = compute_statistics(numberset, setlength);
     float epsilon = 0.001;
-    REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
-    REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
-    REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
+    REQUIRE(abs(computedStats.average - 4.525) < epsilon);
+    REQUIRE(abs(computedStats.max - 8.9) < epsilon);
+    REQUIRE(abs(computedStats.min - 1.5) < epsilon);
 }
 
 TEST_CASE("average is NaN for empty array") {
-    auto computedStats = Statistics::ComputeStatistics({});
-    float NaN = 1.0;    
-    REQUIRE(computedStats.average == NaN);
-    REQUIRE(computedStats.max == NaN);
-    REQUIRE(computedStats.min == NaN);
+    Stats computedStats = compute_statistics(0, 0);
+    //All fields of computedStats (average, max, min) must be
+    //NAN (not-a-number), as defined in math.h
+    
+    //Design the REQUIRE statement here.
+    REQUIRE(computedStats.average != computedStats.average);
+    REQUIRE(computedStats.max != computedStats.max);
+    REQUIRE(computedStats.min != computedStats.min);
+    //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
-    Alerts::EmailAlert emailAlert;
-    Alerts::LEDAlert ledAlert;
-    std::vector<Alerts::IAlerter*> alerters = {&emailAlert, &ledAlert};
-    
-    const float maxThreshold = 10.2;
-    Alerts::StatsAlerter statsAlerter(maxThreshold, alerters);
-    statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
+    // create additional .c and .h files
+    // containing the emailAlerter, ledAlerter functions
+    alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 
-    REQUIRE(emailAlert.emailSent);
-    REQUIRE(ledAlert.ledGlows);
+    float numberset[] = {99.8, 34.2, 4.5};
+    int setlength = sizeof(numberset) / sizeof(numberset[0]);
+    Stats computedStats = compute_statistics(numberset, setlength);
+
+    const float maxThreshold = 10.2;
+    check_and_alert(maxThreshold, alerters, computedStats);
+
+    // need a way to check if both emailAlerter, ledAlerter were called
+    // you can define call-counters along with the functions, as shown below
+    REQUIRE(emailAlertCallCount == 1);
+    REQUIRE(ledAlertCallCount == 1);
 }
